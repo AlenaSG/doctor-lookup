2c407ddd5431b49d6a8aec6696b0906585266dd1
AlenaSG <alenasgolovina@gmail.com>
2017-08-09 06:47:34 -0700

    Update doctor-lookup-interface.js

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,27 +1,10 @@
-var DoctorLookup = require('./../js/doctor-lookup.js').doctorModule;
-
-
-var displayDoctors = function(data) {
-  // if (docData.length<1){
-  //   $('#showDoctors').text("no doctors found");
-  // } else {
-    $('#showDoctors').empty();
-    data.forEach(function(doctor){
-    $('#showDoctors').append('<li>' + "call " + doctor.profile.last_name + '</li>');
-
-  });
-};
-
+var Doctor = require('./../js/doctor-lookup.js').doctorModule;
 
 $(document).ready(function() {
-
-
   $('#find-doctor').click(function() {
-    var medicalIssue = $('#medicalIssue').val();
-    //$('#medicalIssue').val("");
-    var docObject = new DoctorLookup();
 
-    docObject.getDoctors(medicalIssue);
+    var medicalIssue = $('#medicalissue').val();
+    var doctorData = new Doctor();
+    doctorData.getDoctors(medicalIssue, doctorData);
   });
-
-    });
+});
