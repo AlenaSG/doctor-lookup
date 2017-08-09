3bf5ffa2228559021d5d08f0a44eee5fd9813457
Alena Golovina <alenasgolovina@gmail.com>
2017-08-09 09:24:05 -0700

    fix not a function error

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,10 +1,10 @@
-var Doctor = require('./../js/doctor-lookup.js').doctorModule;
+var DoctorLookup = require('./../js/doctor-lookup.js').doctorModule;
 
 $(document).ready(function() {
   $('#find-doctor').click(function() {
 
-    var medicalIssue = $('#medicalissue').val();
-    var doctorData = new Doctor();
-    doctorData.getDoctors(medicalIssue, doctorData);
+    var medicalIssue = $('#medicalIssue').val();
+    var doctor = new DoctorLookup();
+    doctor.getDoctors(medicalIssue, doctor);
   });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,24 +1,24 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(){
+function DoctorLookup(){
 }
-Doctor.prototype.displayDoctorList = function(dataLength, resultsData) {
+DoctorLookup.prototype.displayDoctors = function(dataLength, resultData) {
   var output = [];
   for (var i = 0; i < dataLength; i++) {
-    output.push(resultsData[i]);
+    output.push(resultData[i]);
     console.log("results data:" + output[i]);
   }
   return output;
 };
 
-Doctor.prototype.getDoctors = function(medicalIssue, doctorData) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.6062%2C-122.3321%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(results) {
-      console.log(results);
-      console.log(results.data[0]);
-      console.log(results.data.length);
-      console.log(results.meta);
-      var doctorNames = doctorData.displayDoctorList(results.data.length, results.data);
+DoctorLookup.prototype.getDoctors = function(medicalIssue, doctor) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+      console.log(result.data[0]);
+      console.log(result.data.length);
+      console.log(result.meta);
+      var doctorNames = doctor.displayDoctors(result.data.length, result.data);
       $('#showDoctors').empty();
       doctorNames.forEach(function(name) {
         $('#showDoctors').append('<li>' + name.profile.first_name + ' ' + name.profile.last_name + ", " + name.profile.title + '</li>');
@@ -30,4 +30,4 @@ Doctor.prototype.getDoctors = function(medicalIssue, doctorData) {
     });
 };
 
-exports.doctorModule = Doctor;
+exports.doctorModule = DoctorLookup;
