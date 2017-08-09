e02f0546163d052cb6c979981cc3d0ffd2917912
Alena Golovina <alenasgolovina@gmail.com>
2017-08-09 10:38:17 -0700

    add styling

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -2,11 +2,11 @@ var apiKey = require('./../.env').apiKey;
 
 function DoctorLookup(){
 }
-DoctorLookup.prototype.displayDoctors = function(dataLength, resultData) {
+DoctorLookup.prototype.displayDoctors = function(resultLength, resultData) {
   var output = [];
-  for (var i = 0; i < dataLength; i++) {
+  for (var i = 0; i < resultLength; i++) {
     output.push(resultData[i]);
-    console.log("results data:" + output[i]);
+    console.log("resultData:" + output[i]);
   }
   return output;
 };
@@ -15,9 +15,7 @@ DoctorLookup.prototype.getDoctors = function(medicalIssue, doctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
-      console.log(result.data[0]);
       console.log(result.data.length);
-      console.log(result.meta);
       var doctorContacts = doctor.displayDoctors(result.data.length, result.data);
       $('#showDoctors').empty();
       doctorContacts.forEach(function(contact) {
